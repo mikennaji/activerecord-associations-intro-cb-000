26cd9ad2c8679284d5ad68e36234b19b3f18d99d
Michael Nnaji <mnnaji3@uwo.ca>
2020-10-25 23:08:25 +0000

    Automatically backed up by Learn

--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,7 +1,5 @@
 class AddArtistToSongs < ActiveRecord::Migration[4.2]
-  def change
-    add_foreign_key :artists, :songs
-    add_foreign_key :genres, :songs
-  end
-
+  def change 
+    add_foreign_key :artists, :songs, column: :artist_id, primary_key: "id"
+    add_foreign_key :genres, :songs, column: :genre_id, primary_key: "id"
 end
