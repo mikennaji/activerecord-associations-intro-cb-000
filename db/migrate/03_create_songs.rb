8a85b9d42969a0e7d83c4e16589502513ce08049
Michael Nnaji <mnnaji3@uwo.ca>
2020-10-25 22:54:25 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -2,6 +2,8 @@ class CreateSongs < ActiveRecord::Migration[4.2]
   def change
     create_table :songs do |t|
       t.string :name
+      t.integer :genre_id
+      t.integer :artist_id
     end
   end
 end
